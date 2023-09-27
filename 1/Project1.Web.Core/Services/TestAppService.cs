254e12fd3b9b32db9210e9ca2ff87f49f49e3b59
liufuxin <42953066+Axinyp@users.noreply.github.com>
2023-09-27 14:37:36 +0800

    Add files via upload

---
+++ 1/Project1.Web.Core/Services/TestAppService.cs
@@ -0,0 +1,23 @@
+﻿using Furion.DynamicApiController;
+using Furion.FriendlyException;
+using Furion.JsonSerialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Web.Core.Services
+{
+    public class TestAppService : IDynamicApiController
+    {
+        [LoggingMonitor]
+        public async Task<dynamic> GetDynamic()
+        {
+            string json = "{\"name\":\"张三\",\"age\":18}";
+            var result = JSON.Deserialize<object>(json);
+            return result;
+
+        }
+    }
+}
---
+++ 1/Project1.Web.Core/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Furion;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Project1.Web.Core
+{
+    public class Startup : AppStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddConsoleFormatter();
+
+            services.AddCorsAccessor();
+
+            services.AddControllers()
+                    .AddInjectWithUnifyResult();
+            services.AddMonitorLogging();
+           
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCorsAccessor();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseInject(string.Empty);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
